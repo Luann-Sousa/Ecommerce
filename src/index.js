3b1702b974511be729c4458d2cd0695fee474bf2
Luann244 <weversonluan17@gmail.com>
2021-12-03 23:44:16 -0300

    finaly:  conexao com banco de dados foi finalizada

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
-
+require("dotenv").config();
+const mongoose = require("mongoose");
 const app = express();
 
+//conexão com banco de dados.
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connected to database !"))
+  .catch((error) => console.log("Erroru !", error));
+
+//iniciando o servidor
 app.listen(2222, () => console.log("Start running !"));
-console.log("Heloww Programador");
