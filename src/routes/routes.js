69d26a873cdf5494b2fbf257b590ef62fd7c0c6e
Luann244 <weversonluan17@gmail.com>
2021-12-04 00:20:22 -0300

    create: estrutura de rotas foram montadodas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,4 +9,23 @@ router.get("/index", (request, response) => {
   });
 });
 
+//USUÁRIOS
+router.post("/users"); // criar usuários
+router.get("/users"); // buscar usuários
+router.get("/users/:user_id"); // buscar usuário especifico
+router.post("/login"); // fazer login do usuário
+
+//PRODUTOS
+router.post("/product/:user_id"); //criar pruduto(passar o ud para sabermos quem foi que crio o produto)
+router.get("/products"); // listagem de todos os produtos
+router.get("/products/:user_id"); // buscar produto de um usuario especifico
+router.get("/products/:product_id"); // buscar um produto especifico
+router.patch("/products/user_id/:product_id"); // atualizar um produto
+router.delete("/products/:user_id/:product_id"); // deletar o produto tem que manda id_user e id_product
+
+//CARD/COMPRA
+router.post("/cart/:user_id"); // fazer um comprar
+router.get("/cart/user_id"); // todos os cartão de um usuário
+router.get("/cart/:cart_id"); // bucar cartão
+
 module.exports = router; // exportando nosso arquivo de rotas
